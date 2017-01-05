1a872d7f985acc1ecbe2ebd5990dd3d5f503bc23
Phil Elson <pelson.pub@gmail.com>
2017-01-05 23:42:54 +0000

    Added automatic rising detection, and a linear extrapolation kernel.

--- stratify/__init__.py
+++ stratify/__init__.py
@@ -2,5 +2,6 @@ from __future__ import absolute_import
 
 from ._vinterp import (interpolate,
                        INTERPOLATE_LINEAR, INTERPOLATE_NEAREST,
-                       EXTRAPOLATE_NAN, EXTRAPOLATE_NEAREST)
+                       EXTRAPOLATE_NAN, EXTRAPOLATE_NEAREST,
+                       EXTRAPOLATE_LINEAR)
 
