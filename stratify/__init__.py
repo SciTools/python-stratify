c87fb50a652ccff39566fa2f57e783ec444dd9b9
Carwyn Pelley <cpelley.pub@gmail.com>
2017-10-31 13:02:44 +0000

    TEST: Flake8 F401 error ignore

--- stratify/__init__.py
+++ stratify/__init__.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
 
-from ._vinterp import (interpolate, interp_schemes, extrap_schemes,
-                       INTERPOLATE_LINEAR, INTERPOLATE_NEAREST,
+from ._vinterp import (interpolate, interp_schemes,  # noqa: F401
+                       extrap_schemes, INTERPOLATE_LINEAR, INTERPOLATE_NEAREST,
                        EXTRAPOLATE_NAN, EXTRAPOLATE_NEAREST,
-                       EXTRAPOLATE_LINEAR, PyFuncExtrapolator, 
+                       EXTRAPOLATE_LINEAR, PyFuncExtrapolator,
                        PyFuncInterpolator)
-from ._bounded_vinterp import interpolate as interpolate_conservative
+from ._bounded_vinterp import (interpolate as  # noqa: F401
+    interpolate_conservative)
 
 
 __version__ = '0.1a3.dev0'
