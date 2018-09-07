ed5c6e8d85fe172ab0e728b29c104ddecc783cb5
lbdreyer <laura.dreyer@metoffice.gov.uk>
2018-09-07 11:40:01 +0100

    Add pip install, pytest and coveralls

--- setup.py
+++ setup.py
@@ -11,13 +11,17 @@ from Cython.Build import cythonize
 NAME = 'stratify'
 DIR = os.path.abspath(os.path.dirname(__file__))
 
+extension_kwargs = {'include_dirs': [np.get_include()]}
+cython_coverage_enabled = os.environ.get('CYTHON_COVERAGE', None)
+if cython_coverage_enabled:
+    extension_kwargs.update({'define_macros': [('CYTHON_TRACE_NOGIL', '1')]})
+
 extensions = [Extension('{}._vinterp'.format(NAME),
                         [os.path.join(NAME, '_vinterp.pyx')],
-                        include_dirs=[np.get_include()]),
+                        **extension_kwargs),
               Extension('{}._conservative'.format(NAME),
                         [os.path.join(NAME, '_conservative.pyx')],
-                        include_dirs=[np.get_include()])]
-
+                        **extension_kwargs)]
 
 def extract_version():
     version = None
@@ -37,7 +41,8 @@ setup_args = dict(
                  'Nd vertical interpolation/stratification of atmospheric '
                  'and oceanographic datasets'),
     version=extract_version(),
-    ext_modules=cythonize(extensions),
+    ext_modules=cythonize(extensions, compiler_directives={'linetrace': True,
+                                                           'binding': True}),
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
