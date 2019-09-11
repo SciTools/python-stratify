b03b1b94ab6bffd8059669dafa05c1d2616d8e5d
Tom Gale <tom@tjtg.net>
2019-09-11 12:59:04 +0200

    Fix installation via setup.py and pip (take 2) (#29)
    PEP 517/518 support for setup.py

--- setup.py
+++ setup.py
@@ -1,48 +1,85 @@
 from __future__ import absolute_import, division, print_function
 
-from setuptools import setup, find_packages, Extension
+import glob
 import os
+import sys
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 
-import numpy as np
+# Python 2 has a different name for builtins.
+try:
+    import builtins
+except ImportError:
+    import __builtin__ as builtins
 
-from Cython.Build import cythonize
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    print('Warning: Cython is not available - '
+          'will be unable to build stratify extensions')
+    cythonize = None
+
+PACKAGE_NAME = 'stratify'
+PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
+CMDS_NOCYTHONIZE = ['clean', 'sdist']
 
 
-NAME = 'stratify'
-DIR = os.path.abspath(os.path.dirname(__file__))
+class NumpyBuildExt(_build_ext):
+    # Delay numpy import so that setup.py can be run without numpy already
+    # being installed.
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        builtins.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
-extension_kwargs = {'include_dirs': [np.get_include()]}
-cython_coverage_enabled = os.environ.get('CYTHON_COVERAGE', None)
-if cython_coverage_enabled:
-    extension_kwargs.update({'define_macros': [('CYTHON_TRACE_NOGIL', '1')]})
-
-extensions = [Extension('{}._vinterp'.format(NAME),
-                        [os.path.join(NAME, '_vinterp.pyx')],
-                        **extension_kwargs),
-              Extension('{}._conservative'.format(NAME),
-                        [os.path.join(NAME, '_conservative.pyx')],
-                        **extension_kwargs)]
 
 def extract_version():
     version = None
-    fname = os.path.join(DIR, NAME, '__init__.py')
+    fname = os.path.join(PACKAGE_DIR, PACKAGE_NAME, '__init__.py')
     with open(fname) as fd:
         for line in fd:
-            if (line.startswith('__version__')):
+            if line.startswith('__version__'):
                 _, version = line.split('=')
                 version = version.strip()[1:-1]  # Remove quotations
                 break
     return version
 
 
+# Python 2 is not supported by numpy as of version 1.17
+# but pip will attempt to install/use newer Python 3-only numpy versions.
+numpy_req = 'numpy<1.17' if sys.version_info.major < 3 else 'numpy'
+requirements = ['setuptools>=40.8.0', numpy_req, 'Cython']
+
+extension_kwargs = {}
+cython_directives = {'binding': True}
+cython_coverage_enabled = os.environ.get('CYTHON_COVERAGE', None)
+if cythonize and cython_coverage_enabled:
+    extension_kwargs.update({'define_macros': [('CYTHON_TRACE_NOGIL', '1')]})
+    cython_directives.update({'linetrace': True})
+
+extensions = []
+for source_file in glob.glob('{}/*.pyx'.format(PACKAGE_NAME)):
+    source_file_nosuf, _ = os.path.splitext(os.path.basename(source_file))
+    extensions.append(
+        Extension('{}.{}'.format(PACKAGE_NAME, source_file_nosuf),
+                  sources=[source_file], **extension_kwargs))
+
+if cythonize and not any([arg in CMDS_NOCYTHONIZE for arg in sys.argv]):
+    extensions = cythonize(extensions, compiler_directives=cython_directives)
+
+
 setup_args = dict(
-    name=NAME,
+    name=PACKAGE_NAME,
     description=('Vectorized interpolators that are especially useful for '
                  'Nd vertical interpolation/stratification of atmospheric '
                  'and oceanographic datasets'),
+    author='UK Met Office',
+    author_email='scitools-iris-dev@googlegroups.com',
+    url='https://github.com/SciTools-incubator/python-stratify',
     version=extract_version(),
-    ext_modules=cythonize(extensions, compiler_directives={'linetrace': True,
-                                                           'binding': True}),
+    cmdclass={'build_ext': NumpyBuildExt},
+    ext_modules=extensions,
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -56,13 +93,16 @@ setup_args = dict(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: GIS',
     ],
     extras_require={'test': ['nose']},
-    test_suite='{}.tests'.format(NAME),
+    setup_requires=requirements,
+    install_requires=requirements,
+    test_suite='{}.tests'.format(PACKAGE_NAME),
     zip_safe=False,
 )
 
