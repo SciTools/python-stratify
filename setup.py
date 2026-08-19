c35ccf13755e82715f9395963f14fd6644f9aabc
Henry Wright <henrywright@sky.com>
2026-08-19 10:34:35 +0100

    add regex to accept the license header

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+# Copyright python-stratify contributors
+#
+# This file is part of python-stratify and is released under the BSD license.
+# See LICENSE in the root of the repository for full licensing details.
 import os
 from pathlib import Path
 import sys
